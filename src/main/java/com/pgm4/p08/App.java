15a336aba402c8c206bfd2d87328d1b1b9979c8c
jaisonnn111 <jaisonspeaks@gmail.com>
2025-06-05 10:00:43 +0530

    first commit

---
+++ src/main/java/com/pgm4/p08/App.java
@@ -0,0 +1,13 @@
+package com.pgm4.p08;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
